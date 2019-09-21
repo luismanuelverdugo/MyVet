b15f75ae7fa3b5c8eb19c14fb892c1a19c7c2550
Luis Manuel Verdugo <luis_verper@hotmail.com>
2019-09-21 17:19:57 -0600

    web entties

---
+++ MyVet.Web/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyVet.Web.Data.Entities;
+
+namespace MyVet.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options):base(options)
+        {
+        }
+
+        public DbSet<Owner> Owners { get; set; }
+    }
+}
