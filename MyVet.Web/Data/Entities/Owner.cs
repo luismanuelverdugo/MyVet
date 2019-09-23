cb427c1796233ec7e88b20157aed6e9dc9eec8d4
Luis Manuel Verdugo <luis_verper@hotmail.com>
2019-09-22 20:16:16 -0600

    clases 01

--- MyVet.Web/Data/Entities/Owner.cs
+++ MyVet.Web/Data/Entities/Owner.cs
@@ -9,26 +9,32 @@ namespace MyVet.Web.Data.Entities
     public class Owner
     {
         public int Id { get; set; }
-        [Required]
-        [MaxLength(30)]
+
+        [Display(Name = "Document")]
+        [MaxLength(30, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
         public string Document { get; set; }
-        [Required]
-        [MaxLength(50)]
+
+
         [Display(Name = "First Name")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
         public string FirstName { get; set;}
-        [Required]
-        [MaxLength(50)]
+
         [Display(Name = "Last Name")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
         public string LastName { get; set; }
-        [Required]
-        [MaxLength(20)]
+
         [Display(Name = "Fixed Phone")]
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         public string FixedPhone { get; set; }
-        [Required]
-        [MaxLength(20)]
+
         [Display(Name = "Cell Phone")]
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         public string CellPhone { get; set; }
-        [MaxLength(100)]
+
+        [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         public string Address { get; set; }
 
         [Display(Name = "Owner")]
